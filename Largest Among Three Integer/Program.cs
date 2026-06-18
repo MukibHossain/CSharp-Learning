1c19d955faeec83d6965554f0f809d4c25bb1297
MukibHossain <mukib.11990@gmail.com>
2026-06-19 00:47:59 +0600

    Added largest projecrt

---
+++ Largest Among Three Integer/Program.cs
@@ -0,0 +1,17 @@
+﻿using system;
+class program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter another number: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter last number: ");
+        int num3 = Convert.ToInt32(Console.ReadLine());
+
+        int largest
+    }
+}
