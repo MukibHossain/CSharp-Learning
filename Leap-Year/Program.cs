de2c9893d65318ffad953220abdcef5d024f9aac
MukibHossain <mukib.11990@gmail.com>
2026-06-18 21:35:03 +0600

    Added leap year project

---
+++ Leap-Year/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a year: ");
+        int year = Convert.ToInt32(Console.ReadLine());
+
+        // Leap year checking condition
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+        {
+            Console.WriteLine("Leap Year");
+        }
+        else
+        {
+            Console.WriteLine("Not a Leap Year");
+        }
+    }
+}
