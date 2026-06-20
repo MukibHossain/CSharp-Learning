6406aaf6cfecd6b95bd481f495b108f6e63d7f40
MukibHossain <mukib.11990@gmail.com>
2026-06-20 10:59:52 +0600

    Test quadrant added

---
+++ Determine Quadrant/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int x = int.Parse(Console.ReadLine());
+        int y = int.Parse(Console.ReadLine());
+
+        if (x > 0 && y > 0)
+            Console.WriteLine("1st Quadrant");
+        else if (x < 0 && y > 0)
+            Console.WriteLine("2nd Quadrant");
+        else if (x < 0 && y < 0)
+            Console.WriteLine("3rd Quadrant");
+        else if (x > 0 && y < 0)
+            Console.WriteLine("4th Quadrant");
+        else
+            Console.WriteLine("On Axis");
+    }
+}
