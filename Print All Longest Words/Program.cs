c415e23aabdff8f3220fbb523605db3edd80ddbf
MukibHossain <mukib.11990@gmail.com>
2026-06-20 10:55:53 +0600

    Print LOngest words added

---
+++ Print All Longest Words/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a sentence: ");
+        string sentence = Console.ReadLine();
+        string[] words = sentence.Split(' ');
+        int maxLength = 0;
+        foreach (string word in words)
+        {
+            if (word.Length > maxLength)
+                maxLength = word.Length;
+        }
+        Console.WriteLine("Longest words:");
+        foreach (string word in words)
+        {
+            if (word.Length == maxLength)
+                Console.WriteLine(word);
+        }
+    }
+
+}
+
