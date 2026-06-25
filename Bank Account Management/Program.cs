fa9b59f63c8c9dc0c0b543b88948439fbfd9378f
MukibHossain <mukib.11990@gmail.com>
2026-06-25 22:10:09 +0600

    bank account class and method added

---
+++ Bank Account Management/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class BankAccount
+{
+    private int balance = 0;
+
+    public void Deposit(int amount)
+    {
+        balance = balance + amount;
+        Console.WriteLine("Deposited: " + amount);
+    }
+
+    public void Withdraw(int amount)
+    {
+        if (amount <= balance)
+        {
+            balance = balance - amount;
+            Console.WriteLine("Withdrawn: " + amount);
+        }
+        else
+        {
+            Console.WriteLine("Not enough balance");
+        }
+    }
+
+    public void CheckBalance()
+    {
+        Console.WriteLine("Balance: " + balance);
+    }
+}
+
