568ba1980aa7ba8febda12605b00727e0764b947
MukibHossain <mukib.11990@gmail.com>
2026-06-25 22:08:47 +0600

    main promgram added

--- Tesla Self-Driving System/Program.cs
+++ Tesla Self-Driving System/Program.cs
@@ -16,4 +16,30 @@ class TeslaCar
     }
 }
 
+class Program
+{
+    static void Main()
+    {
+        TeslaCar car = new TeslaCar();
 
+        Console.Write("Enter Road Condition: (Highway/Urban/Rain) ");
+        string road = Console.ReadLine();
+
+        if (road == "Highway")
+        {
+            car.SetAutonomy(AutonomyLevel.Autonomous);
+        }
+        else if (road == "Urban")
+        {
+            car.SetAutonomy(AutonomyLevel.AssistedDriving);
+        }
+        else if (road == "Rain")
+        {
+            car.SetAutonomy(AutonomyLevel.EmergencyOverride);
+        }
+        else
+        {
+            Console.WriteLine("Wrong Input");
+        }
+    }
+}
