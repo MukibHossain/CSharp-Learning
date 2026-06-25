f9489ee2934c07020cd7915337c88b14b010b96a
MukibHossain <mukib.11990@gmail.com>
2026-06-25 22:08:25 +0600

    enum values are added

---
+++ Tesla Self-Driving System/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+enum AutonomyLevel
+{
+    Manual,
+    AssistedDriving,
+    Autonomous,
+    EmergencyOverride
+}
+
+class TeslaCar
+{
+    public void SetAutonomy(AutonomyLevel level)
+    {
+        Console.WriteLine("Driving Mode: " + level);
+    }
+}
+
+
