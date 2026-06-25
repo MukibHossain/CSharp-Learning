3f6d2d9f72a62328c178d52fb25cb955fc69f1d2
MukibHossain <mukib.11990@gmail.com>
2026-06-25 22:06:44 +0600

    initial class added

---
+++ Neuralink State Control/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+enum ImplantMode
+{
+    Idle,
+    DataProcessing,
+    NeuroStimulation,
+    SleepMode
+}
+
+class NeuralinkDevice
+{
+    public void SwitchMode(ImplantMode mode)
+    {
+        Console.WriteLine("Current Mode: " + mode);
+    }
+}
+
+
+
