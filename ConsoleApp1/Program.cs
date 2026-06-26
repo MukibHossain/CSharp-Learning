15e9fde7cf74e0f99cb51a7b6cf17edaa272fbfe
MukibHossain <mukib.11990@gmail.com>
2026-06-27 00:18:44 +0600

    Prime Number Checker Added

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,25 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter a number:");
+            int n = int.Parse(Console.ReadLine());
+            int count = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    count++;
+                }
+            }
+            if (count == 2)
+            {
+                Console.WriteLine("Prime number.");
+            }
+            else
+            {
+                Console.WriteLine("Not Prime Number");
+            }
         }
     }
 }
