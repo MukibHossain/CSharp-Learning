8375a55b9d835ae6cd0cd71ed9c8f5fde7ef1c6b
MukibHossain <mukib.11990@gmail.com>
2026-06-29 12:41:34 +0600

    Student class with private fields StudentID, Name and CGPA code is added

--- Indexer1/Program.cs
+++ Indexer1/Program.cs
@@ -10,6 +10,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -25,6 +26,66 @@ class student
         get { return studentID; }
         set { studentID = value; }
     }
-    
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+    //property for cgpa
+
+    public double CGPA
+    { 
+        get {return cgpa; }
+        set
+        {
+            if(value >= 0.00 && value <= 4.00)
+            {
+                cgpa = value;
+            }
+            else
+            {
+                cgpa = 0.00;
+            }
+        }
+    }
+
+    //studentlist class creation
+
+    class StudentList
+    {
+        private student[] students = new student[5];
+
+        //indexer to access students by index
+        public student this[int index]
+        {
+            get { return students[index]; }
+            set { students[index] = value; }
+        }
+    }
+    // class main
+    class Program
+    {
+        static void Main()
+        {
+            StudentList list = new StudentList();
+
+            // 5 students info
+
+            list[0] = new student { StudentID = 101, Name = "Mukib", CGPA = 3.5 };
+            list[1] = new student { StudentID = 102, Name = "Odhi", CGPA = 4.0 };
+            list[2] = new student { StudentID = 103, Name = "OdhiJr", CGPA = 3.98 };
+            list[3] = new student { StudentID = 104, Name = "MukibJr", CGPA = 3.99 };
+            list[4] = new student { StudentID = 105, Name = "Odhini", CGPA = 3.89 };
+
+            Console.WriteLine("Studnet Info\n");
+
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Student ID: {list[i].StudentID}, Name: {list[i].Name}, CGPA: {list[i].CGPA}");
+            }
+        }
+    }
+
 }
 
