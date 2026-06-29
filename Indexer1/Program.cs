fc6cfd2be778abe828a056e1d1c2d41614c50d61
MukibHossain <mukib.11990@gmail.com>
2026-06-29 12:00:57 +0600

    student class properties created

---
+++ Indexer1/Program.cs
@@ -0,0 +1,30 @@
+﻿//Question 3: Properties and Indexers
+//Create a Student class with private fields StudentID, Name and CGPA.
+//Use properties to access all fields.
+//Validate CGPA so it remains between 0.00 and 4.00 (otherwise set it to 0.00).
+//Create a StudentList class that stores five Student objects in an array and implements an
+//indexer to access students by index.
+//Display all student information using the indexer.
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class student
+{
+    //private fields
+    private int studentID;
+    private string name;
+    private double cgpa;
+
+    public int StudentID
+    {
+        get { return studentID; }
+        set { studentID = value; }
+    }
+    
+}
+
