a08a1b2b48fc750e396d1fa9c682e06c9ece6d52
MukibHossain <mukib.11990@gmail.com>
2026-06-30 21:01:52 +0600

    single inheritance added

---
+++ A draf code just for testing/Program.cs
@@ -0,0 +1,62 @@
+﻿// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+
+public class Student
+{
+    int studentId;
+    string name;
+    double cgpa;
+    public int StudentId { get; set; }
+    public string Name { get; set; }
+    public double CGPA
+    {
+        get
+        {
+            return cgpa;
+        }
+        set
+        {
+            if (value > 0.0 && value <= 4.0)
+            {
+                cgpa = value;
+            }
+            else
+            {
+                cgpa = 0.0;
+            }
+        }
+    }
+    public Student(int sId, string n, double cg)
+    {
+        StudentId = sId;
+        Name = n;
+        CGPA = cg;
+
+    }
+}
+class StudentList
+{
+    private Student[] students = new Student[2];
+
+    public Student this[int index]
+    {
+        get { return students[index]; }
+        set { students[index] = value; }
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        StudentList list = new StudentList();
+
+        list[0] = new Student(23, "X", 4.0);
+        list[1] = new Student(24, "Y", 5.0);
+
+        Console.WriteLine(list[0].StudentId + " " +
+                          list[0].Name + " " +
+                          list[0].CGPA);
+    }
+}
---
+++ Inheritance All/Q1 - Single Inheritance/Program.cs
@@ -0,0 +1,51 @@
+﻿//Create two classes: Person and Student.The Person class should have the properties Name
+//and Age, and a method DisplayInfo() that prints the name and age. The Student class should
+//inherit from Person and have an additional property StudentID and method
+//DisplayStudentInfo() that prints the student ID along with the name and age. Demonstrate
+//single inheritance by creating a Student object.
+
+using System;
+
+class Person
+{
+    // Properties
+    public string Name;
+    public int Age;
+
+    // Method to display person info
+    public void DisplayInfo()
+    {
+        Console.WriteLine("Name: " + Name);
+        Console.WriteLine("Age: " + Age);
+    }
+}
+
+class Student : Person
+{
+    // Additional property
+    public int StudentID;
+
+    // Method to display student info
+    public void DisplayStudentInfo()
+    {
+        DisplayInfo(); // inherited method
+        Console.WriteLine("Student ID: " + StudentID);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Creating Student object
+        Student s = new Student();
+
+        // Assigning values
+        s.Name = "Rahim";
+        s.Age = 20;
+        s.StudentID = 101;
+
+        // Displaying information
+        s.DisplayStudentInfo();
+    }
+}
