9385c15c1e15f2cc7ad9b5bb5906e3a1abf1b262
MukibHossain <mukib.11990@gmail.com>
2026-06-30 21:07:33 +0600

    another hierarchical code added

---
+++ Inheritance All/Q4 - Multiple Inheritance (via Interfaces)/Program.cs
@@ -0,0 +1,45 @@
+﻿//Define two interfaces: IAthlete and IMusician.IAthlete should have a method PlaySport() and
+//IMusician should have a method PlayInstrument(). Then create a class Person that
+//implements both interfaces. Write a program that demonstrates multiple inheritance using
+//interfaces by creating a Person object that can play a sport and an instrument.
+
+using System;
+
+// First interface
+interface IAthlete
+{
+    void PlaySport();
+}
+
+// Second interface
+interface IMusician
+{
+    void PlayInstrument();
+}
+
+// Person class implements both interfaces
+class Person : IAthlete, IMusician
+{
+    public void PlaySport()
+    {
+        Console.WriteLine("Person is playing football");
+    }
+
+    public void PlayInstrument()
+    {
+        Console.WriteLine("Person is playing guitar");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Creating Person object
+        Person p = new Person();
+
+        // Calling methods
+        p.PlaySport();
+        p.PlayInstrument();
+    }
+}
