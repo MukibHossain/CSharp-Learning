693a10b8016f2f947118509c97843bf4e987167e
MukibHossain <mukib.11990@gmail.com>
2026-07-03 21:41:04 +0600

    remove Duplicates from an Array added

---
+++ Remove Duplicates from an Array/Program.cs
@@ -0,0 +1,15 @@
+﻿//Write a C# program that removes all duplicates from a given array of integers and prints the resulting
+//array.
+
+using System;
+using System.Linq;
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 4, 8, 4, 6, 8, 2, 9, 6, 1 };
+        Console.WriteLine("Original Array: " + string.Join(", ", numbers));
+        int[] uniqueNumbers = numbers.Distinct().ToArray();
+        Console.WriteLine("Array Without Duplicates: " + string.Join(", ", uniqueNumbers));
+    }
+}
