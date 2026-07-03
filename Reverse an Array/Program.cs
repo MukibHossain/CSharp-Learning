9b7149360323c08d253faa4ac3c1f88d8723c9b5
MukibHossain <mukib.11990@gmail.com>
2026-07-03 21:39:48 +0600

    reverse an array added

---
+++ Reverse an Array/Program.cs
@@ -0,0 +1,15 @@
+﻿//Write a C# program that takes an array of integers as input, reverses the order of elements in the array,
+//and prints the reversed array.
+
+using System;
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 10, 20, 30, 40, 50 };
+        Console.WriteLine("Original Array: " + string.Join(", ", numbers));
+        Array.Reverse(numbers);
+        Console.WriteLine("Reversed Array: " + string.Join(", ", numbers));
+    }
+}
+
