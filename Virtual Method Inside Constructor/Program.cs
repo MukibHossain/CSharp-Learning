20323f94ddfe0ff99c948cb8b917eed419cfa3e8
MukibHossain <mukib.11990@gmail.com>
2026-07-06 00:00:00 +0600

    virtual method inside contructor added

---
+++ Virtual Method Inside Constructor/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Parent
+{
+    public Parent()
+    {
+        Show();
+    }
+
+    public virtual void Show()
+    {
+        Console.WriteLine("Parent");
+    }
+}
+
+class Child : Parent
+{
+    int x = 100;
+
+    public override void Show()
+    {
+        Console.WriteLine(x);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Child c = new Child();
+    }
+}
