2551da5e8595882bfa8f7d205ea548be1e10b3a2
MukibHossain <mukib.11990@gmail.com>
2026-07-05 23:56:34 +0600

    chain flow added

---
+++ Chain Flow/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class A
+{
+    public A()
+    {
+        Console.WriteLine("A()");
+    }
+
+    public A(int x)
+    {
+        Console.WriteLine("A(int)");
+    }
+}
+
+class B : A
+{
+    public B() : this(5)
+    {
+        Console.WriteLine("B()");
+    }
+
+    public B(int x) : base(x)
+    {
+        Console.WriteLine("B(int)");
+    }
+}
+
+class C : B
+{
+    public C() : base()
+    {
+        Console.WriteLine("C()");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        new C();
+    }
+}
