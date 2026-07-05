ddda0c20976fc110eec8c43ab8d67861456d93ac
MukibHossain <mukib.11990@gmail.com>
2026-07-06 00:04:12 +0600

    Can You Find the Error? added

---
+++ Missing Base/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class A
+{
+    public A(int x)
+    {
+        Console.WriteLine(x);
+    }
+}
+
+class B : A
+{
+    public B()
+    {
+        Console.WriteLine("B");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        B b = new B();
+    }
+}
