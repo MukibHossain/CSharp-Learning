6175d75e449bf00a850d087b4f7b138563b98967
MukibHossain <mukib.11990@gmail.com>
2026-07-06 00:02:43 +0600

    Constructor Overloading + this() + base() added

---
+++ Mixed Chaining/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class X
+{
+    public X()
+    {
+        Console.WriteLine("X()");
+    }
+
+    public X(int a)
+    {
+        Console.WriteLine("X(int)");
+    }
+}
+
+class Y : X
+{
+    public Y() : this(10)
+    {
+        Console.WriteLine("Y()");
+    }
+
+    public Y(int a) : base(a)
+    {
+        Console.WriteLine("Y(int)");
+    }
+}
+
+class Z : Y
+{
+    public Z() : base()
+    {
+        Console.WriteLine("Z()");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Z obj = new Z();
+    }
+}
