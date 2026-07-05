802c015c5b7ae464b73c6dcbceb4cbd34133269a
MukibHossain <mukib.11990@gmail.com>
2026-07-05 23:59:08 +0600

    static constructor added

---
+++ Static Constructor/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Parent
+{
+    static Parent()
+    {
+        Console.WriteLine("Parent Static");
+    }
+
+    public Parent()
+    {
+        Console.WriteLine("Parent Instance");
+    }
+}
+
+class Child : Parent
+{
+    static Child()
+    {
+        Console.WriteLine("Child Static");
+    }
+
+    public Child()
+    {
+        Console.WriteLine("Child Instance");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Child c1 = new Child();
+        Console.WriteLine("-----");
+        Child c2 = new Child();
+    }
+}
