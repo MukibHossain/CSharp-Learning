3931cb1dbb6eb5c8964334a024a2d8086120ebbc
MukibHossain <mukib.11990@gmail.com>
2026-07-15 13:37:08 +0600

    Full code added

---
+++ Interface and abstract/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+interface IVehicle
+{
+    void Start();
+    void Stop();
+}
+class Car : IVehicle
+{
+    public void Start()
+    {
+        Console.WriteLine("Car starting");
+    }
+
+    public void Stop()
+    {
+        Console.WriteLine("Car stopping");
+    }
+}
+class Bike : IVehicle
+{
+    public void Start()
+    {
+        Console.WriteLine("Bike starting");
+    }
+
+    public void Stop()
+    {
+        Console.WriteLine("Bike stopping");
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Car car = new Car();
+        Bike bike = new Bike();
+
+        car.Start();
+        car.Stop();
+
+        Console.WriteLine();
+
+        bike.Start();
+        bike.Stop();
+    }
+}
