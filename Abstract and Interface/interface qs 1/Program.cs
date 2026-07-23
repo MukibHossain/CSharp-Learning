12bad381f6f64e558b78a6fdbbefbb27ffc4f241
MukibHossain <mukib.11990@gmail.com>
2026-07-23 21:53:02 +0600

    full code added

---
+++ Abstract and Interface/interface qs 1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+interface IEmployee
+{
+    void AssignEmail();
+    void AssignManager();
+}
+
+class PermanentEmployee : IEmployee
+{
+    public void AssignEmail()
+    {
+        Console.WriteLine("Permanent Employee Email Assigned.");
+    }
+
+    public void AssignManager()
+    {
+        Console.WriteLine("Permanent Employee Manager Assigned.");
+    }
+}
+
+class ContractualEmployee : IEmployee
+{
+    public void AssignEmail()
+    {
+        Console.WriteLine("Contractual Employee Email Assigned.");
+    }
+
+    public void AssignManager()
+    {
+        Console.WriteLine("Contractual Employee Manager Assigned.");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Interface Reference
+        IEmployee permanent = new PermanentEmployee();
+        IEmployee contractual = new ContractualEmployee();
+
+        Console.WriteLine("Permanent Employee:");
+        permanent.AssignEmail();
+        permanent.AssignManager();
+
+        Console.WriteLine();
+
+        Console.WriteLine("Contractual Employee:");
+        contractual.AssignEmail();
+        contractual.AssignManager();
+
+        Console.ReadKey();
+    }
+}
