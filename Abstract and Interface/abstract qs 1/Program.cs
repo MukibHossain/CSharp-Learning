60ca697ec8f3e6170e2d8175361b25c2626cf319
MukibHossain <mukib.11990@gmail.com>
2026-07-23 21:40:45 +0600

    full code added

---
+++ Abstract and Interface/abstract qs 1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+//creating asbtract class
+public abstract class EmployeeDress
+{
+    public abstract void DressCode();
+
+    public void DressColor()
+    {
+        Console.WriteLine("Dress Color: BLUE");
+    }
+}
+class PermanentEmployee : EmployeeDress
+{
+    public override void DressCode()
+    {
+        Console.WriteLine("Formal Office Wear");
+    }
+}
+class ContractualEmployee : EmployeeDress
+{
+    public override void DressCode()
+    {
+        Console.WriteLine("Smart Casual");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        EmployeeDress emp1 = new PermanentEmployee();
+        emp1.DressCode();
+        emp1.DressColor();
+        EmployeeDress emp2 = new ContractualEmployee();
+        emp2.DressCode();
+        emp2.DressColor();
+    }
+}
