dbd502203cd13f59cce19006d32e84923e8145e8
MukibHossain <mukib.11990@gmail.com>
2026-07-26 23:51:48 +0600

    opening handling added

---
+++ Exception/Opening Errors/Program.cs
@@ -0,0 +1,31 @@
+﻿//Write a C# program to handle file opening errors.
+
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.Write("Enter a number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int result = 100 / num;
+            Console.WriteLine("Result = " + result);
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Error: Division by zero.");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Error: Invalid input. Please enter a number.");
+        }
+        finally
+        {
+            Console.WriteLine("Program finished.");
+        }
+    }
+}
---
+++ Exception/Opening Errors/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Opening Errors")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Opening Errors")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6c7f9530-b5ef-49f5-9a4d-b7ca4ec9d792")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
