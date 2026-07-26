9cd87e3df20b669b03d56d1df22ac40a09266940
MukibHossain <mukib.11990@gmail.com>
2026-07-26 23:52:58 +0600

    half code added

---
+++ Exception/IndexOutOfRangeException/Program.cs
@@ -0,0 +1,17 @@
+﻿//Write a C# program to handle IndexOutOfRangeException.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            int[] numbers = { 10, 20, 30 };
+
+            Console.Write("Enter array index: ");
+            int index = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Value = " + numbers[index]);
+        }
