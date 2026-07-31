ff14ec4a37893d05ae3cf1a40d0bb9590b1c69a4
MukibHossain <mukib.11990@gmail.com>
2026-07-31 23:56:26 +0600

    code added

---
+++ Aggregation/Person and Age/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Address
+{
+    public string Street;
+    public string City;
+
+    public Address(string street, string city)
+    {
+        Street = street;
+        City = city;
+    }
+}
+
+class Person
+{
+    public string Name;
+
+    public Address Address;
+
+    public Person(string name, Address address)
+    {
+        Name = name;
+        Address = address;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine("Name: " + Name);
+        Console.WriteLine("Street: " + Address.Street);
+        Console.WriteLine("City: " + Address.City);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Address a = new Address("Uttara", "Dhaka");
+
+        Person p = new Person("Hasan", a);
+
+        p.Display();
+    }
+}
