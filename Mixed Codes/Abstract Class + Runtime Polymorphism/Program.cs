615c1c72de9c2f609673701e29d0e26ea7a0b3cd
MukibHossain <mukib.11990@gmail.com>
2026-08-05 22:39:24 +0600

    employee class body created

---
+++ Mixed Codes/Abstract Class + Runtime Polymorphism/Program.cs
@@ -0,0 +1,37 @@
+﻿//Question 1: Employee Payroll System (Abstract Class + Runtime Polymorphism)
+//Scenario:
+//A software company wants to automate its monthly payroll system. All employees have
+//common information such as Employee ID, Name, and Basic Salary, but different employee
+//types receive different allowances.
+//Requirements:
+//1.Create an abstract class Employee with properties EmployeeID, Name, BasicSalary; a
+//constructor; DisplayInfo(); and abstract CalculateSalary().
+//2.Create Manager(HRA = 30 %, Medical = 20 %) and SoftwareEngineer(HRA= 20 %,
+//Performance Bonus = 15 %) classes.
+//3.Override CalculateSalary().
+//4.In Main(), store one Manager and one SoftwareEngineer in an Employee[] array and use
+//runtime polymorphism to display information and calculated salary.
+
+using System;
+public abstract class  Employee
+{
+    public int EmployeeID { get; set; }
+    public string Name { get; set; }
+    public double BasicSalary { get; set; }
+
+    public Employee(int employeeID, string name, double basicSalary)
+    {
+        EmployeeID = employeeID;
+        Name = name;
+        BasicSalary = basicSalary;
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine("Employee ID: " + EmployeeID);
+        Console.WriteLine("Employee Name: " + Name);
+        Console.WriteLine("Employee Salary: " + BasicSalary);
+    }
+
+    public abstract double CalculateSalary();
+}
