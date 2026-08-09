5915f6c8b704fadaf7302aeb3588c1557289b79e
MukibHossain <mukib.11990@gmail.com>
2026-08-09 23:35:20 +0600

    product class code added

---
+++ Mixed Codes/Online Shopping Billing/Program.cs
@@ -0,0 +1,24 @@
+﻿//using System;
+
+//Create a C# program for an online shopping system that accepts product price and quantity, then calculates and displays the total bill.
+
+//The program must handle the following exceptions:
+
+//FormatException — when the user enters non-numeric input.
+//OverflowException — when the entered number is outside the range of the data type.
+//ArgumentOutOfRangeException — when price or quantity is less than or equal to 0. This exception must be thrown manually.
+using System;
+class Product
+{
+    public double Price { get; set; }
+    public int Quantity { get; set; }
+    public double TotalAmount { get; set; }
+
+    public void Display()
+    {
+        Console.WriteLine("\nPrice : " + Price);
+        Console.WriteLine("Quantity : " + Quantity);
+        Console.WriteLine("Total Amount : " + TotalAmount);
+    }
+}
+
---
+++ Practice/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+abstract class Employee
+{
+    public string Name { get; set; }
+
+    public Employee(string name)
+    {
+        Name = name;
+    }
+
+    public abstract void CalculateBonus();
+
+    public void DisplayEmployee()
+    {
+        Console.WriteLine("Employee Name: " + Name);
+    }
+}
+
+class Manager : Employee
+{
+    public Manager(string name) : base(name)
+    {
+    }
+
+    public override void CalculateBonus()
+    {
+        Console.WriteLine("Manager bonus: 20% of salary");
+    }
+}
+
+class Developer : Employee
+{
+    public Developer(string name) : base(name)
+    {
+    }
+
+    public override void CalculateBonus()
+    {
+        Console.WriteLine("Developer bonus: 10% of salary");
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Manager manager = new Manager("Emon Mahmud");
+        Developer developer = new Developer("Mukib Hossain");
+
+        manager.DisplayEmployee();
+        manager.CalculateBonus();
+
+        Console.WriteLine();
+
+        developer.DisplayEmployee();
+        developer.CalculateBonus();
+    }
+}
