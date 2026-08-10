7e77c1ecbb1d2286377a2d2d1d38a001efb2d909
MukibHossain <mukib.11990@gmail.com>
2026-08-10 23:45:55 +0600

    Main program added

--- Mixed Codes/Food Payment ( Delegate)/Program.cs
+++ Mixed Codes/Food Payment ( Delegate)/Program.cs
@@ -13,7 +13,6 @@
 //● Display a payment confirmation and 'Order Confirmed Successfully.' 
 
 using System;
-
 delegate void PaymentDelegate(double amount);
 class Payment
 {
@@ -23,20 +22,58 @@ class Payment
         Console.WriteLine("Amount: " + amount);
         Console.WriteLine("Payments will be collected upon delivery.");
     }
-
     public static void BkashPayment(double amount)
     {
         Console.WriteLine("Payment Method: Bkash");
         Console.WriteLine("Amount: " + amount);
         Console.WriteLine("Payment will be processed through Bkash.");
     }
-
     public static void CreditCardPayment(double amount)
     {
         Console.WriteLine("Payment Method: Card");
         Console.WriteLine("Amount: " + amount);
         Console.WriteLine("Payment will be processed through Card.");
     }
+}
 
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter Food Bill: ");
+        double amount = Convert.ToDouble(Console.ReadLine());
 
+        Console.WriteLine("\n1. Cash on Delivery");
+        Console.WriteLine("2. bKash");
+        Console.WriteLine("3. Credit Card");
+
+        Console.Write("Choose Payment Method: ");
+        int choice = Convert.ToInt32(Console.ReadLine());
+
+        PaymentDelegate payment = null;
+
+        if (choice == 1)
+        {
+            payment = Payment.CashOnDelivery;
+        }
+        else if (choice == 2)
+        {
+            payment = Payment.BkashPayment;
+        }
+        else if (choice == 3)
+        {
+            payment = Payment.CreditCardPayment;
+        }
+        else
+        {
+            Console.WriteLine("Invalid Choice.");
+            return;
+        }
+
+        Console.WriteLine();
+
+        payment(amount);
+
+        Console.WriteLine("Order Confirmed Successfully.");
+    }
 }
