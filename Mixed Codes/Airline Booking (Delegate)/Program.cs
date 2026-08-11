53f296205002696fed6489691bec12ba43406144
MukibHossain <mukib.11990@gmail.com>
2026-08-11 10:43:55 +0600

    delegate created

---
+++ Mixed Codes/Airline Booking (Delegate)/Program.cs
@@ -0,0 +1,52 @@
+﻿//Airline Ticket Booking System (Delegates + Events) 
+//Problem Statement: 
+//Develop an Airline Ticket Booking System. Customers can pay using Credit Card, Debit Card, or bKash. After 
+//successful payment, the system should automatically: 
+//• Send the e - ticket by email 
+//• Send an SMS confirmation 
+//• Update seat availability 
+//Requirements: 
+//Part A(Delegate)
+//● Define PaymentDelegate. 
+//● Create:
+//• CreditCardPayment(double amount)
+//• DebitCardPayment(double amount)
+//• BkashPayment(double amount)
+//● Ask the user to enter ticket price. 
+//● Ask the user to choose a payment method. 
+//● Use the delegate to invoke the selected payment method. 
+//Part B (Event)
+//● Create a publisher class named Airline.
+//● Declare an event named TicketBooked. 
+//● Create subscriber methods: 
+//• SendEmail()
+//• SendSMS()
+//• UpdateSeatAvailability()
+//● Raise the event after successful payment.
+
+using System;
+delegate void PaymentDelegate(double amount);
+class Payment
+{
+    public static void CreditCardPayment(double amount)
+    {
+        Console.WriteLine("Payment by Credit Card Successful.");
+        Console.WriteLine("Amount Paid: " + amount);
+    }
+
+    public static void DebitCardPayment(double amount)
+    {
+        Console.WriteLine("Payment by Debit Card Successful.");
+        Console.WriteLine("Amount Paid: " + amount);
+    }
+
+    public static void BkashPayment(double amount)
+    {
+        Console.WriteLine("Payment by bKash Successful.");
+        Console.WriteLine("Amount Paid: " + amount);
+    }
+}
+
+
+delegate void TicketHandler();
+
