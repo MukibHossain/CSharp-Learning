8b783885b82a5eac97794d4093bb72a097c064fc
MukibHossain <mukib.11990@gmail.com>
2026-08-29 21:01:41 +0600

    delegates 2 added

---
+++ Delegate/Delegates 2/Program.cs
@@ -0,0 +1,28 @@
+﻿//Write a delegate CalculateCircleArea that takes a double parameter (radius) and returns a
+//double (the area of the circle). Write a method GetArea that calculates the area of a circle
+//using the formula π * radius ^ 2.Create an instance of the delegate and use it to calculate and
+//print the area for a circle with a radius of 7.
+//Code: 
+using System;
+
+class Program
+{
+    delegate double CalculateCircleArea(double radius);
+
+    static double GetArea(double radius)
+    {
+        return Math.PI * radius * radius;
+    }
+
+    static void Main()
+    {
+        CalculateCircleArea calculate = GetArea;
+
+        double radius = 7;
+
+        double area = calculate(radius);
+
+        Console.WriteLine("Area = " + area);
+    }
+}
+
