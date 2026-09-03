71c82d5971fd820cab07fd0b69522b08dbccf0b6
MukibHossain <mukib.11990@gmail.com>
2026-09-04 00:02:36 +0600

    code added linq 6

---
+++ Linq/Linq 6/Program.cs
@@ -0,0 +1,29 @@
+﻿//Write a C# LINQ query to find and display all the scores greater than 80 from the following array:
+//int[] scores = { 97, 92, 81, 60 };
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        // Specify the data source
+        int[] scores = { 97, 92, 81, 60 };
+
+        // Define the LINQ query
+        var scoreQuery =
+            from i in scores
+            where i > 80
+            select i;
+
+        // Execute the query
+        Console.WriteLine("Scores greater than 80:");
+
+        foreach (var i in scoreQuery)
+        {
+            Console.Write(i + " ");
+        }
+
+        Console.ReadLine();
+    }
+}
